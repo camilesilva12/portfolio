1a39e52274b1692178cc2d6604c15490869fb6e5
camilesilva12 <camiledaconceicaosilva38@gmail.com>
2025-07-10 21:04:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    const id = link.getAttribute('href').substring(1);
+    const destino = document.getElementById(id);
+
+    // Esconde todas as seções
+    document.querySelectorAll('.secao').forEach(secao => {
+      secao.classList.remove('ativa');
+    });
+
+    // Mostra apenas a seção clicada
+    destino.classList.add('ativa');
+
+    // Rola suavemente para o topo
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
 
